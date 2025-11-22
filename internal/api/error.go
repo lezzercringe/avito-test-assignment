a7406958da0311b23d6c526ca5f4cf3a0954de75
Alexander Goncharov <lezzercringe@gmail.com>
2025-11-23 02:56:41 +0300

    Extract error codes into enum

--- internal/api/error.go
+++ internal/api/error.go
@@ -7,6 +7,17 @@ import (
 	"go.uber.org/zap"
 )
 
+type ErrorCode string
+
+const (
+	CodeNotFound    ErrorCode = "NOT_FOUND"
+	CodeTeamExists  ErrorCode = "TEAM_EXISTS"
+	CodePRExists    ErrorCode = "PR_EXISTS"
+	CodePRMerged    ErrorCode = "PR_MERGED"
+	CodeNotAssigned ErrorCode = "NOT_ASSIGNED"
+	CodeNoCandidate ErrorCode = "NO_CANDIDATE"
+)
+
 func RespondJSON(w http.ResponseWriter, v any) {
 	body, err := json.Marshal(v)
 	if err != nil {
